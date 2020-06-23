c02b966d37eae8adcda5c752ed2e4132a74d97e0
akovalev <akovalev@rtech.ru>
2020-06-23 21:18:19 +0300

    Пока не собирается

--- vncServer/vncServer.c
+++ vncServer/vncServer.c
@@ -1,10 +1,14 @@
 #include <rfb/rfb.h>
 
+const int WIDTH = 1600;
+const int HEIGHT = 900;
+
 int main(int argc,char** argv)
 {
-  rfbScreenInfoPtr server=rfbGetScreen(&argc,argv,400,300,8,3,4);
-  server->frameBuffer=malloc(400*300*4);
-  rfbInitServer(server);
-  rfbRunEventLoop(server,-1,FALSE);
+  rfbScreenInfoPtr screen=rfbGetScreen(&argc,argv, WIDTH, HEIGHT,8,3,4);
+  screen->frameBuffer=malloc(WIDTH * HEIGHT * 4);
+  rfbInitServer(screen);
+  rfbRunEventLoop(screen,-1,FALSE);
+  rfbMarkRectAsModified(screen, 0, 0, WIDTH, HEIGHT);
   return(0);
 }
