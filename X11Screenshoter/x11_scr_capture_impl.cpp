ec9238e5efc117529d37f328dc66eca7eabd92c4
kovalexius <saneck2ru@mail.ru>
2020-07-14 10:22:23 +0300

    Наделал распечаток в X11 capturer, потому что падает в корку, нашел что в корку падает в части VNC

--- X11Screenshoter/x11_scr_capture_impl.cpp
+++ X11Screenshoter/x11_scr_capture_impl.cpp
@@ -2,10 +2,11 @@
 
 #include <iostream>
 
-X11ScrCapturerImpl::X11ScrCapturerImpl()
+X11ScrCapturerImpl::X11ScrCapturerImpl() : m_display(NULL)
 {
 	initX11();
 }
+
 X11ScrCapturerImpl::~X11ScrCapturerImpl()
 {
 	if(m_display)
@@ -29,19 +30,28 @@ void X11ScrCapturerImpl::initX11()
 	{
 		std::cout << "failed XGetWindowAttributes()" << std::endl;
 	}
+	else 
+	{
+		std::cout << "width: " << m_attributes.width <<
+					"height: " << m_attributes.height <<std::endl;
+	}
 }
 
 bool X11ScrCapturerImpl::getScreenshot(const CRectangle& _region, 
 						std::vector<char>& _outBuffer)
 {
+	std::cout << __FUNCTION__ << std::endl;
+
+	/*
 	XImage *image = XGetImage(m_display,
 								m_root,
 								0, 0,
 								m_attributes.width, m_attributes.height,
 								AllPlanes,
 								ZPixmap);
+	*/
 
-	/*
+	//*
 	XImage *image = XGetImage(m_display, 
 								m_root, 
 								_region.getLeftBottom().m_x, 
@@ -50,7 +60,15 @@ bool X11ScrCapturerImpl::getScreenshot(const CRectangle& _region,
 								_region.getSize().m_y, 
 								AllPlanes, 
 								ZPixmap);
-								*/
+	/**/
+
+	if(image == NULL)
+	{
+		std::cout << "XGetImage failed " << std::endl;
+		return false;
+	}
+
+	std::cout << "XGetImage success" <<std::endl;
 
 	_outBuffer.resize(m_attributes.width * m_attributes.height * _region.getBytesPerPixel());
 
